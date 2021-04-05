f495b0e149656f3188b261b33bbde91bf966ed48
Gglecinilson Silva <glecinilson@gmail.com>
2021-04-05 19:00:22 -0400

    Login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,76 @@
-import Head from 'next/head'
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <h1>DashGO</h1>
+    <Flex
+      width='100vw'
+      height='100vh'
+      alignItems='center'
+      justifyContent='center'
+
+    >
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth='360px'
+        backgroundColor='gray.800'
+        padding='8'
+        borderRadius='8'
+        flexDirection='column'
+      >
+
+       <Stack
+        spacing='4'
+       >
+         <FormControl>
+          <FormLabel htmlFor='email'>
+            E-mail
+          </FormLabel>
+
+            <Input
+              id='email'
+              name='email'
+              type='email'
+              focusBorderColor='pink.500'
+              backgroundColor='gray.900'
+              variant='filled'
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+              size='lg'
+            />
+        </FormControl> 
+        <FormControl>
+            <FormLabel htmlFor='password'>
+              Password
+            </FormLabel>
+
+
+            <Input
+              id='password'
+              name='password'
+              type='password'
+              focusBorderColor='pink.500'
+              backgroundColor='gray.900'
+              variant='filled'
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+              size='lg'
+            />
+        </FormControl> 
+       </Stack>
+
+        <Button
+          type='submit'
+          marginTop='6'
+          size='lg'
+          colorScheme='pink'
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+    </Flex>
   )
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,8 +3,13 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   styles: {
     global: {
+      fonts: {
+        body: 'Poppins',
+        heading: 'Poppins'
+      },
       body: {
-        
+        background: 'gray.900',
+        color: 'gray.50'
       }
     }
   }
